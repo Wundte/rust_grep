130f4ea8a02bb4f8fdd629cbdbce0ca37250bc2a
wundte <wundte@gmail.com>
2023-09-28 20:03:40 +0200

    Reading from file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     
     let query = &args[1];
     let file_path = &args[2];
+    
+    let content = fs::read_to_string(file_path).expect("Was unable to read from file");
 }
