a1562a2b3675f0ba79ea31ff354639b04bcde67b
wundte <wundte@gmail.com>
2023-10-12 10:25:19 +0200

    Refactoring chapter 12.3 rust book, part 1

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,23 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
     
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::new(&args);
     
-    let content = fs::read_to_string(file_path).expect("Was unable to read from file");
+    // println!("running with: {}, {}", config.query, config.file_path);
+    
+    let content = fs::read_to_string(config.file_path).expect("Was unable to read from file");
+}
+
+struct Config {
+    pub query: String,
+    pub file_path: String
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Config {query, file_path}
+    }
 }
